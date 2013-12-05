a64faf30e5f586c59e6451195be98ea5b50091f9
Bohdan.Kul <bohdan.kul@gmail.com>
2013-12-05 00:33:01 -0500

    Random base object providing access to real and integer uniform distributions.

---
+++ randombase.cpp
@@ -0,0 +1,18 @@
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/variate_generator.hpp>
+
+#include "randombase.h"
+using namespace std;
+
+RandomBase::RandomBase(long seed):
+eng(1),uReal(0,1),uInt(0,65535231),uRandInt(eng,uInt),uRand(eng,uReal)
+{    
+    //Initialize ranndomness with the seed
+    uRand.engine().seed(seed);
+    uRand.distribution().reset();
+    uRandInt.engine().seed(seed);
+    uRandInt.distribution().reset();
+}
+
---
+++ randombase.h
@@ -0,0 +1,28 @@
+#ifndef RANDOMBASE_H
+#define RANDOMBASE_H
+
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/variate_generator.hpp>
+
+using namespace std;
+
+class RandomBase {
+    public:
+        //Define data-types of random objects 
+        typedef boost::mt19937 t_eng;                 //Mersenne twister
+        typedef boost::uniform_real<float> t_uReal;   //Uniform real distribution
+        typedef boost::uniform_int<int>    t_uInt;    //Uniform longeger distribution
+
+        //Random generation objects
+        t_eng   eng;                 //Mersenne twister
+        t_uReal uReal;               //Uniform real distribution
+        t_uInt  uInt;                //Uniform distribution 
+
+        boost::variate_generator<t_eng&, t_uInt > uRandInt; //Generator of integers
+        boost::variate_generator<t_eng&, t_uReal> uRand;    //Generator of real number
+    
+        RandomBase(long seed);
+};
+#endif
